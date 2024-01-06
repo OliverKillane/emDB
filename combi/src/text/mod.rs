5cbf047aa27618cf3e150229aecfa48a436ef49a
Oliver Killane <oliverkillane@gmail.com>
2024-01-06 23:46:53 +0000

    added report template

--- combi/src/text/mod.rs
+++ combi/src/text/mod.rs
@@ -1,7 +1,12 @@
-//! A small text parsing library that allows for complex recovery, and context sensitivity.
+//! A small text parsing library atop combi that allows for complex recovery, and context sensitivity.
 //!
 //! <div class="warning">This part of the library is unimplemented, as it is not useful for the core goal of the emdb project</div>
 //!
+//!
+//! Support complex recovery.
+//! - Combi already supports recovery that can succeed (e.g. use a different successful parser),
+//!   continue or error to fall through to another outer recovery
+//! - With text there are more options to backtrack, than with tokenstreams
 //! ```ignore
 //! // try to parse "hello",
 //! // if unable re-try with "hullo",
@@ -22,26 +27,6 @@
 //!     word("world"),
 //! )
 //! ```
-//!
-//! ```ignore
-//! // Support context based parsing (e.g. C++)
-//! // for example
-//! select(
-//!     getword(),
-//!     (english, french, german, dutch),
-//!     |txt, (english, french, german, dutch)| match &txt {
-//!         "hello" => english,
-//!         "bonjour" => french,
-//!         "hallo" => recover(
-//!             german,
-//!             backtrack(dutch)
-//!         )
-//!     },
-//!     |_| "english, french, dutch or german",
-//! )
-//! ```
-//!
-//! // fallthrough recovery
 //! ```ignore
 //! // can recover inside , lists, or outer list of statements ,,,;,,,;
 //! seplist(
@@ -60,11 +45,41 @@
 //!                 ),
 //!                 letter(','),
 //!             ),
-//!             |s| Statement::from(s)
+//!             Statement::from
 //!         ),
 //!         skipto(isletter(','))
 //!     ),
 //!     letter(';')
+//! )
+//! ```
+//!
+//! Support context based parsing fully using select.
+//! - Combi's current select is entirely stack allocated, but does use a vtable (we can improve upon this)
+//! ```ignore
+//! select(
+//!     getword(),
+//!     (english, french, german, dutch),
+//!     |txt, (english, french, german, dutch)| match &txt {
+//!         "hello" => english,
+//!         "bonjour" => french,
+//!         "hallo" => recover(
+//!             german,
+//!             backtrack(dutch)
+//!         )
+//!     },
+//!     |_| "english, french, dutch or german",
+//! )
+//! ```
+//!
+//! Support changes in input stream type.
+//! - Combis can have different output that input, so we can map between different representations where convenient.
+//! ```ignore
+//! diffseq(
+//!    tokenize(...),
+//!    seq(
+//!         word("hello"),
+//!         space,
+//!     )
 //! )
 //! ```
 
